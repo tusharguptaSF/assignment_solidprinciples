e7731d024aed08ee8fa54a4dbaa36a31ab39c13e
Amisha Gupta <amisha.gupta@stackroute.in>
2021-04-01 12:35:01 +0530

    Update Text.ts

---
+++ excercise1/Text.ts
@@ -0,0 +1,16 @@
+class Text {
+    mytext:string;
+    author:string;
+    length:number;
+    getMytext(){ return "" }
+    setMytext(s:string) {  }
+    getAuthor() { return ""; }
+    setAuthor(s:string) {  }
+    getLength() { return 0; }
+    setLength(k:number) { }
+    /*method for printing text*/
+    printText() {  }
+    /*methods that change the text*/
+    allLettersToUpperCase() {  }
+    findSubTextAndDelete(String s) {  }
+}
